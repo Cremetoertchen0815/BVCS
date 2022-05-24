f2564d7cd2ae64ca35be800b830569ed7dcb314a
Mirco Heck <m.heck@gebhardt-group.com>
2022-05-24 10:11:39 +0200

    Reverted back to template

---
+++ Betreten Verboten/GameCore.cs
@@ -0,0 +1,33 @@
+﻿using GeonBit;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Betreten_Verboten
+{
+	class GameCore : GeonBitGame
+	{
+		public GameCore()
+		{
+			InitParams.Title = "New Project";
+		}
+
+		public override void Draw(GameTime gameTime)
+		{
+			
+		}
+
+		public override void Initialize()
+		{
+
+		}
+
+		public override void Update(GameTime gameTime)
+		{
+			
+		}
+	}
+}
--- Betreten Verboten/Program.cs
+++ Betreten Verboten/Program.cs
@@ -1,50 +1,20 @@
-﻿using GeonBit.ECS;
-using GeonBit.ECS.Components.Graphics;
-using GeonBit.UI.Entities;
-using Microsoft.Xna.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace Betreten_Verboten
 {
-	class Lol : GeonBit.GeonBitGame
-	{
-		public override void Draw(GameTime gameTime)
-		{
-			
-		}
-
-		public override void Initialize()
-		{
-			ActiveScene.UserInterface.AddEntity(new Paragraph("This is a fucking test text!"));
-
-			var soos = new GameObject("camera");
-			soos.AddComponent(new Camera());
-			soos.SceneNode.PositionZ = 5;
-			soos.Parent = ActiveScene.Root;
-
-
-			/// Example 3: add 3d shape to scene
-			GameObject shapeObject = new GameObject("shape");
-			shapeObject.AddComponent(new ShapeRenderer(ShapeMeshes.Sphere));
-			shapeObject.Parent = ActiveScene.Root;
-
-		}
-
-		public override void Update(GameTime gameTime)
-		{
-			
-		}
-	}
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			var lol = new Lol();
-			GeonBit.GeonBitMain.Instance.Run(new Lol());
-		}
-	}
+    /// <summary>
+    /// Program entry point.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // execute GeonBit with Game1 instance.
+            GeonBit.GeonBitMain.Instance.Run(new GameCore());
+        }
+    }
 }
