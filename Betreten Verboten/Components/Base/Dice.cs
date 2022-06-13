99e26177943c4aa694dc9e58e3e2bd06af0ed497
Mirco Heck <m.heck@gebhardt-group.com>
2022-06-13 14:51:59 +0200

    lol

--- Betreten Verboten/Components/Base/Dice.cs
+++ Betreten Verboten/Components/Base/Dice.cs
@@ -54,11 +54,8 @@ namespace Betreten_Verboten.Components.Base
 		{
 			var floatDots = new (float dot, int index)[_sideNormals.Length]; //Store dice face dot products with their indices
 			var norm = Vector3.TransformNormal(Vector3.Up, Matrix.Invert(_rigidBody.Entity.Node.WorldTransformations)); //Translate target normal into object space
-																														//Calculate dot product between translated target normal and surface normal. Bigger means closer to the target normal.
-			for (int i = 0; i < _sideNormals.Length; i++)
-			{
-				floatDots[i] = (Vector3.Dot(norm, _sideNormals[i]), i);
-			}
+			//Calculate dot product between translated target normal and surface normal. Bigger means closer to the target normal.
+			for (int i = 0; i < _sideNormals.Length; i++) floatDots[i] = (Vector3.Dot(norm, _sideNormals[i]), i);
 			//Effeciently calculate the face that is closest to matching the target normal and return the face index.
 			return floatDots.Aggregate((x, y) => x.dot > y.dot ? x : y).index + 1;
 		}
