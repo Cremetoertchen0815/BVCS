a94ccebe49a7f5360d0228dd19fe096e8d82f7fb
Creme <Mircosoft_heck@web.de>
2022-07-01 16:40:47 +0200

    Started working on GeonBit.UI-Nez interface

---
+++ Nez.GeonBit/ECS/Renderers/UIRenderer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Nez.GeonBit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nez.GeonBit
+{
+    public class UIRenderer : Renderer
+    {
+        private UserInterface _ui;
+        private SpriteBatch _spr;
+
+        public UIRenderer(int renderOrder) : base(renderOrder) => _ui = UserInterface.Active;
+        public override void Render(Scene scene)
+        {
+            _ui.Draw(null);
+        }
+    }
+}
