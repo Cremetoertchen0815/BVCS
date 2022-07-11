dfcbae93e6bb24c66d7bab4cb55a027e9290724c
Creme <Mircosoft_heck@web.de>
2022-07-11 08:59:02 +0200

    Added debug cam mover

---
+++ Betreten Verboten/Components/Debug/DebugCamMover.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Betreten_Verboten.Components.Debug
+{
+    internal class DebugCamMover
+    {
+    }
+}
